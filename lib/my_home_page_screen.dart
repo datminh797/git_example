d149a31980e1c07a93704f1825233b28c98171d1
admin <>
2025-01-22 10:12:38 +0700

    add text from main 3

--- lib/my_home_page_screen.dart
+++ lib/my_home_page_screen.dart
@@ -31,7 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               width: double.infinity,
               child: Text(
-                'Text from main 2',
+                'Text from main 3',
                 style: TextStyle(fontSize: 20),
                 textAlign: TextAlign.center,
               ),
